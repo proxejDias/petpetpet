c2cfce6ba33bd978fed6cc8bbe7ff7a98214b320
Yessetov Dias <YessetovDias04@gmail.com>
2025-09-21 19:44:32 +0500

    fourth

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-print("boutta blow")
-print("boutta blow sec")
-print("boutta blow secs")
-print("boutta blow fourth")
+import psycopg2
+from config import host, user, password, db_name
+
+connection = None
+
+try:
+    connection = psycopg2.connect(
+        host = host,
+        user = user,
+        password = password,
+        database = db_name
+    )
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT version();"
+        )
+        print(f"Server version: {cursor.fetchone()}")
+
+except Exception as _ex:
+    print("[INFO] Error while working with PostgreSQL", _ex)
+finally:
+    if connection:
+        connection.close()
+        print("[INFO] PostgreSQL connection closed")
